9a01ecd5c2122fe0b8efed0815a38d29a1840046
Murad685 <orucowmurad@gmail.com>
2021-06-11 16:16:08 +0400

    Task01 add

---
+++ Week01/task01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ele bir program yazin ki, o programda ozunuze aid olan melumatlari gostere bilesiniz.
+            //Adiniz, soyadiniz,yashiniz ve universitet adini consoleda gosterin.
+            //Qeyd:Deyishenlere deyerler hard code sheklinde verilsin.
+            string ad = "Murad";
+            string soyad = "Orucov";
+            int yash = 21;
+            string uni= "ADNSU";
+            string ixtisas = "Informasiya Texnologiyalari";
+
+            Console.WriteLine("Ad: "+ad) ;
+            Console.WriteLine("Soyad: "+soyad) ;
+            Console.WriteLine("Yash: "+ yash) ;
+            Console.WriteLine("Universitet: "+uni) ;
+            Console.WriteLine("Ixtisas: "+ixtisas) ;
+        }
+    }
+}
