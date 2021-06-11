c5d5af80b5220f8dfa40c47304c503ecc6c7fbc4
Murad685 <orucowmurad@gmail.com>
2021-06-11 17:07:40 +0400

    Task04 add

---
+++ Week01/task04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ele bir program yazin ki, qarshinizdaki insandan adini ve soyadini sorushun.
+            //Aldiginiz cavablari deyishenlere menimsedin ve sual verdiyiniz adama adini cekerek consoleda salam deyin.
+            string ad;
+            string soyad;
+            Console.WriteLine("Zehmet olmasa Adinizi daxil edin");
+            ad = Console.ReadLine();
+            Console.WriteLine("Soyadiniz daxil edin:");
+            soyad = Console.ReadLine();
+            Console.WriteLine("Salam " + ad +" " + soyad + " dostum,necesen?");
+            Console.ReadLine();
+        }
+    }
+}
