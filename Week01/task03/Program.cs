5f41764a994bcb2dd780a6cad2647129d0e6f5d4
Murad685 <orucowmurad@gmail.com>
2021-06-11 16:58:16 +0400

    Task03 add

---
+++ Week01/task03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ele bir program yazin ki, o programda 3 herf daxil edin ve o herfleri console-da tersine gosterin.
+            char a = 'a';
+            char b = 'b';
+            char c = 'c';
+            Console.Write(c);
+            Console.Write(b);
+            Console.Write(a);
+        }
+    }
+}
