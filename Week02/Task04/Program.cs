c0b954590fe8283194d216e9d5f6f66acdb512be
Murad685 <orucowmurad@gmail.com>
2021-06-15 22:03:50 +0400

    Week02-Task04 add

---
+++ Week02/Task04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dynamic olaraq daxil edilmish integer deyerin musbet ve ya menfi eded olmasini yoxlayin.
+            int a;
+            Console.WriteLine("Eded daxil et:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (a < 0)
+            {
+                Console.WriteLine("Eded menfidir!");
+            }
+            else
+            {
+                Console.WriteLine("Eded musbetdir!");
+            }
+        }
+    }
+}
