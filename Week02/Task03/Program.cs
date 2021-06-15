44c2fb64ea7e46103af526337d6830c570c8b1f8
Murad685 <orucowmurad@gmail.com>
2021-06-15 21:57:42 +0400

    Week02-Task03 add

---
+++ Week02/Task03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dynamic olaraq daxil edilmish integer deyerin tek ve ya cut eded olmasini yoxlayin.
+            int a;
+            Console.WriteLine("Eded daxil et:");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 1)
+            {
+                Console.WriteLine("Tek ededdir!");
+            }
+            else
+            {
+                Console.WriteLine("Cut ededdir!");
+            }
+        }
+    }
+}
