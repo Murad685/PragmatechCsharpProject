d4e08f4b8b89568b0c6ae1755fff11edba62746e
Murad685 <orucowmurad@gmail.com>
2021-06-21 19:20:59 +0400

    Week03-Task03 add

---
+++ Week03/Task03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task03
+{
+    class Program
+    {
+        /*
+         * Istifadecinin dynamic olaraq daxil etdiyi ededin faktorialini tapin.
+         * Daxil edilen ededin sifir olmasi ehtimalinin qarshisini alin.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded daxil edin:");
+            int eded = Convert.ToInt32(Console.ReadLine());
+            int factor = 1;
+            if (eded==0)
+            {
+                Console.WriteLine("Factorial 0-dir");
+            }
+            for (int i=1; i <= eded ; i++)
+            {
+
+                factor = factor * i; 
+                
+            }
+            Console.WriteLine($"factorial={factor}");
+        }
+    }
+}
