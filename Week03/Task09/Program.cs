8ae687c216304797b17e8c22660fc9c953a6d9c7
Murad685 <orucowmurad@gmail.com>
2021-06-25 18:01:41 +0400

    Week10-Task09 add

---
+++ Week03/Task09/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 2-den 100-e qeder ededler arasinda sade ededleri(1e ve ozune bolunen) tapib ekrana cixardin.
+             */
+            int a = 2;
+            int b = 100;
+            while (a <= b)
+            {
+                int c = 0;
+                int j = 1;
+                while (j <= a)
+                {
+                    if (a % j == 0)
+                    {
+                        c++;
+                    }
+                    j++;
+                }
+
+                if (c == 2)
+                {
+                    Console.WriteLine(a);
+                }
+                a++;
+            }
+        }
+    }
+}
