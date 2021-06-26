5c3fc4c3a5cacad52f085185a46e21937cb9fa7e
Murad685 <orucowmurad@gmail.com>
2021-06-26 16:24:21 +0400

    Week03-Task10 Update

--- Week03/Task10/Program.cs
+++ Week03/Task10/Program.cs
@@ -10,21 +10,50 @@ namespace Task10
              * Client-dan iki eded daxil etmesini isteyin ve daxil edilen ededler arasindaki sade ededleri tapib ekrana cixardin.
              */
 
-            Console.WriteLine("Zehmet olmasa iki eded daxil edin:");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine($"{a} ve {b} arasindaki ededler:");
-            if (a<b)
+            Console.WriteLine("Zehmet olmasa iki eded daxil edin, Ilk kicik eded olsun:");
+            int beginNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("iKinci ve boyuk olan ededi daxil edin:");
+            int endNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{beginNumber} ve {endNumber} arasindaki sade edeler:");
+            Console.WriteLine("_________________________________");
+
+            for (int i = beginNumber; i < endNumber; i++)
+            {
+                if (i is 2 )
+                {
+                    Console.WriteLine(2);
+                }
+                if (i % 2 is not 0)
+                {
+                    int temp = 2;
+                    for (int j = 2; j < i; j++)
+                    {
+
+                        if (i % j is not 0)
+                        {
+                            temp++;
+                            if (temp==i)
+                            {
+                                Console.WriteLine(i);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            /*
+             * Console.WriteLine($"{beginNumber} ve {endNumber} arasindaki ededler:");
+            if (beginNumber < endNumber)
             {
 
-                while (a <= b)
+                while (beginNumber <= endNumber)
                 {
                     int c = 0;
                     int j = 1;
-                    while (j<=a)
+                    while (j<= beginNumber)
                     {
-                        if (a % j == 0)
+                        if (beginNumber % j == 0)
                         {
                             c++;
                         }
@@ -33,20 +62,20 @@ namespace Task10
 
                     if (c == 2)
                     {
-                        Console.WriteLine(a);
+                        Console.WriteLine(beginNumber);
                     }
-                    a++;
+                    beginNumber++;
                 }
             }
             else
             {
-                while (a >=b)
+                while (beginNumber >= endNumber)
                 {
                     int c = 0;
                     int j = 1;
-                    while (j <= a)
+                    while (j <= beginNumber)
                     {
-                        if (a % j == 0)
+                        if (beginNumber % j == 0)
                         {
                             c++;
                         }
@@ -55,11 +84,12 @@ namespace Task10
 
                     if (c == 2)
                     {
-                        Console.WriteLine(a);
+                        Console.WriteLine(beginNumber);
                     }
-                    a--;
+                    beginNumber--;
                 }
             }
+             */
         }
     }
 }
