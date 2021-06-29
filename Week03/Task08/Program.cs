130da36d607bd0130a3792c9c02ab0a6e7034d11
Murad685 <orucowmurad@gmail.com>
2021-06-29 20:40:46 +0400

    Week03-Task08 add

---
+++ Week03/Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Console-a a-dan z-ye qeder herfleri cixardin.
+             */
+            char[] elifba = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}; 
+            foreach (var item in elifba)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
