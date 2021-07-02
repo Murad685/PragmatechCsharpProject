c80c6de6c711626925263a8b992c6097012e48e7
Murad685 <orucowmurad@gmail.com>
2021-07-02 20:02:09 +0400

    Week04-Task2 second method add

---
+++ Week04/Task02 second method/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task02_second_method
+{
+    class Program
+    {
+        //5 olculu int tipde bir array yaradib ona random ededler menimsedin.
+        //Daha sonra hemin arrayin elementlerinin cemini tapin.
+        static void Main(string[] args)
+        {
+            int[] list = new int[5];
+            Random rndm = new Random();
+            int i = 0;
+            int sum = 0;
+            while (i < 5)
+            {
+                list[i] = rndm.Next(0, 5);
+                i++;
+            }
+            
+            Array.ForEach(list, delegate (int i) { sum += i; });
+            Console.WriteLine(sum);
+        }
+    }
+}
