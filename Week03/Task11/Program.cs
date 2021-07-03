6b31220ad13a8bc7d9f7a5121413aa2d8062b745
Murad685 <orucowmurad@gmail.com>
2021-07-03 13:55:02 +0400

    Week03-Task11 add

---
+++ Week03/Task11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While-dan istifade ederek 1-den 100-e qeder olan ededler arasinda yerleshen cut ededlerin ozunu ve cemini console-a cixardin.
+            int i = 1;
+            while (i<=100)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+                i++;
+            }
+        }
+    }
+}
