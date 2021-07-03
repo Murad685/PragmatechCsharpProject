a67d8a0804411d503de2592a4a7236bfc8e32a05
Murad685 <orucowmurad@gmail.com>
2021-07-03 11:04:25 +0400

    Week04-Task05 add

---
+++ Week04/Task05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Task05
+{
+    class Program
+    {
+        //Cut ve tek ededlerden ibaret olan int tipli arrayin icinden cut ededleri silin.
+        static void Main(string[] args)
+        {
+            int[] list = new int[25];
+            Random randomNumber = new Random();
+            int i = 0;
+            while (i < list.Length)
+            {
+                list[i] = randomNumber.Next(0, 25);
+
+                if (list[i] % 2 == 0)
+                {
+                    list = list.Where((source, index) => index != i).ToArray();
+                }
+                i++;
+            }
+              
+            Console.WriteLine("Listin elementler: "+ String.Join(',', list));
+
+           
+
+        }
+    }
+}
