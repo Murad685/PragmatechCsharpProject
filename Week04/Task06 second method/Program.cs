8d136a668ddc005355f04e6001259c96feba05d4
Murad685 <orucowmurad@gmail.com>
2021-07-03 10:14:43 +0400

    Week04-Task06 second method

---
+++ Week04/Task06 second method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+namespace Task06_second_method
+{
+    class Program
+    {
+        //Arrayin minimum elementini tapin.
+        static void Main(string[] args)
+        {
+            int[] ededler = new int[5] { 3, 10, 60, 6, 1 };
+            
+            int minimum = ededler[3];
+
+            for (int i = 0; i < 5; i++)
+            {
+                int listelem = ededler[i];
+
+                if (listelem < minimum)
+                {
+                    minimum = listelem;
+                }
+            }
+
+
+            
+            Console.WriteLine($"Listin minimumu: {minimum}");
+        }
+    }
+}
