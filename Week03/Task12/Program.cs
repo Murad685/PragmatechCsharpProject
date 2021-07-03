4a4cb1494b6201665a51c6561a6b4112d5a0d623
Murad685 <orucowmurad@gmail.com>
2021-07-03 14:01:43 +0400

    Week03-Task12 add

---
+++ Week03/Task12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-den 10-a qeder vurma cedvelini Console-a cixardin.
+            for (int i = 1; i < 11; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    int hasil = j * i;
+                    Console.WriteLine($"{ j} * {i} = {hasil}");
+                }
+            }
+        }
+    }
+}
