0b63d59798bad790f50cce5baf1437d7085bfa62
Murad685 <orucowmurad@gmail.com>
2021-07-03 10:03:15 +0400

    Week04-Task06 first method

---
+++ Week04/Task06 first method/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Task06_first_method
+{
+    class Program
+    {
+        //Arrayin minimum elementini tapin.
+        static void Main(string[] args)
+        {
+            int[] ededler = new int[] { 3, 10, 60, 6, 1 };
+            int min = ededler.Min();
+            Console.WriteLine($"Listin minimumu: {min}");
+        }
+    }
+}
