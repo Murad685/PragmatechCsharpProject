3a2fc9220e6ee042ff5bcfaf6c4010ea06df5035
Murad685 <orucowmurad@gmail.com>
2021-07-03 10:27:29 +0400

    Week04-Task01 second method

---
+++ Week04/Task01 second method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task01_second_method
+{
+    class Program
+    {
+        //5 olculu int tipde bir array yaradib ona random ededler menimsedin.
+        //Daha sonra hemin arrayin elementlerini tersine duzun.
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] { 3, 1, 4, 5, 2 };
+
+            for (int i = 4; i >-1; i--)
+            {
+                Console.WriteLine( array[i]);
+            }
+            
+        }
+    }
+}
