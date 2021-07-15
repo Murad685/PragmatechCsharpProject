78229a01eb9e93a9b32555af4a9d6857c1b58491
Murad685 <orucowmurad@gmail.com>
2021-07-15 18:24:43 +0400

    Week05-Task05 add

---
+++ Week05/Task05/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace Task05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.surname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ponenumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.birthday = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtBoxname = new System.Windows.Forms.TextBox();
+            this.txtBoxSurename = new System.Windows.Forms.TextBox();
+            this.maskedTxtBoxNumber = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dateTimePickerBirthday = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.name,
+            this.surname,
+            this.ponenumber,
+            this.birthday});
+            this.listView.GridLines = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(138, 201);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(317, 172);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // name
+            // 
+            this.name.Text = "Name";
+            this.name.Width = 74;
+            // 
+            // surname
+            // 
+            this.surname.Text = "Surname";
+            this.surname.Width = 71;
+            // 
+            // ponenumber
+            // 
+            this.ponenumber.Text = "PhoneNumber";
+            this.ponenumber.Width = 82;
+            // 
+            // birthday
+            // 
+            this.birthday.Text = "BirthDay";
+            this.birthday.Width = 87;
+            // 
+            // txtBoxname
+            // 
+            this.txtBoxname.Location = new System.Drawing.Point(138, 75);
+            this.txtBoxname.Name = "txtBoxname";
+            this.txtBoxname.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxname.TabIndex = 1;
+            // 
+            // txtBoxSurename
+            // 
+            this.txtBoxSurename.Location = new System.Drawing.Point(299, 75);
+            this.txtBoxSurename.Name = "txtBoxSurename";
+            this.txtBoxSurename.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxSurename.TabIndex = 2;
+            // 
+            // maskedTxtBoxNumber
+            // 
+            this.maskedTxtBoxNumber.Location = new System.Drawing.Point(138, 136);
+            this.maskedTxtBoxNumber.Mask = "(+000 00) 000-00-00";
+            this.maskedTxtBoxNumber.Name = "maskedTxtBoxNumber";
+            this.maskedTxtBoxNumber.Size = new System.Drawing.Size(100, 20);
+            this.maskedTxtBoxNumber.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Adınız:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(296, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Soyadınız:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Telefon nömrə:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Doğum günü:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.Location = new System.Drawing.Point(455, 91);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(158, 65);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Əlavə et";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dateTimePickerBirthday
+            // 
+            this.dateTimePickerBirthday.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerBirthday.Location = new System.Drawing.Point(299, 136);
+            this.dateTimePickerBirthday.Name = "dateTimePickerBirthday";
+            this.dateTimePickerBirthday.Size = new System.Drawing.Size(100, 20);
+            this.dateTimePickerBirthday.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePickerBirthday);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTxtBoxNumber);
+            this.Controls.Add(this.txtBoxSurename);
+            this.Controls.Add(this.txtBoxname);
+            this.Controls.Add(this.listView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ColumnHeader surname;
+        private System.Windows.Forms.ColumnHeader ponenumber;
+        private System.Windows.Forms.ColumnHeader birthday;
+        private System.Windows.Forms.TextBox txtBoxname;
+        private System.Windows.Forms.TextBox txtBoxSurename;
+        private System.Windows.Forms.MaskedTextBox maskedTxtBoxNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBirthday;
+    }
+}
+
---
+++ Week05/Task05/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task05
+{
+    public partial class Form1 : Form
+    {
+        //Listview goturun ve Name,Surname,PhoneNumber,BirthDate adinda columnlar yaradin.
+        //Name ve Surname daxil etmek ucun iki textbox goturun.
+        //PhoneNumber ucun maskedtextbox ve birthdate ucun datetimepicker goturun.
+        //Onlari doldurub Add button-un vuranda listview-nu doldurun.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtBoxname.Text.Trim();
+            string surname = txtBoxSurename.Text.Trim();
+            string phonenum = maskedTxtBoxNumber.Text.Trim();
+            string birthday = dateTimePickerBirthday.Text;
+
+
+            ListViewItem item = new ListViewItem();
+
+            item.Text = name;
+            item.SubItems.Add(surname);
+            item.SubItems.Add(phonenum);
+            item.SubItems.Add(birthday);
+            listView.Items.Add(item);
+
+            txtBoxname.Clear();
+            txtBoxSurename.Clear();
+            maskedTxtBoxNumber.Clear();
+        }
+    }
+}
