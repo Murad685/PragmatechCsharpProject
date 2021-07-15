0b568428939146045c6c591dbb9cb0a3fa65f1dc
Murad685 <orucowmurad@gmail.com>
2021-07-15 17:01:11 +0400

    Week05-Task03 add but continue

---
+++ Week05/Task03/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Task03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnMix = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(266, 171);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(209, 173);
+            this.listBox.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(563, 122);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Əlavə et";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adı daxil edin:";
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(265, 129);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(100, 20);
+            this.txtBox.TabIndex = 3;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(563, 161);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Sil";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnMix
+            // 
+            this.btnMix.Location = new System.Drawing.Point(563, 199);
+            this.btnMix.Name = "btnMix";
+            this.btnMix.Size = new System.Drawing.Size(75, 23);
+            this.btnMix.TabIndex = 5;
+            this.btnMix.Text = "Qarışdır";
+            this.btnMix.UseVisualStyleBackColor = true;
+            this.btnMix.Click += new System.EventHandler(this.btnMix_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMix);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnMix;
+    }
+}
+
---
+++ Week05/Task03/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task03
+{
+    public partial class Form1 : Form
+    {
+        //Listbox goturun ve her defe elave et button-un clickleyende icine adlar daxil edin.
+        //Daha sonra bir button da goturun adini Sil qoyun ve clicklenende listdeki datalari silmek mumkun olsun.
+        //En sonda bir Qarishdir adinda bir button da elave edin ve hemin button clicklenende listbox-un icindeki datalari qarishdirmaq mumkun olsun.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtBox.Text.Trim().ToLower();
+            listBox.Items.Add(name);
+            txtBox.Clear();
+            txtBox.Focus();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Remove(listBox.SelectedItem);
+        }
+
+        private void btnMix_Click(object sender, EventArgs e)
+        {
+            //Random rndm = new Random();
+
+            //int[] Number = new int[listBox.Items.Count];
+
+        }
+    }
+}
