5d5aea25e303f8acf2fa5d802655c08b0726d800
Murad685 <orucowmurad@gmail.com>
2021-07-18 15:46:30 +0400

    Week05-Task01 add

---
+++ Week05/Task01/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Task01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.response = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.Yellow;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(299, 148);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(68, 38);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged_1);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.BackColor = System.Drawing.Color.Yellow;
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown2.Location = new System.Drawing.Point(422, 148);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(68, 38);
+            this.numericUpDown2.TabIndex = 1;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.ForeColor = System.Drawing.Color.Yellow;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "/",
+            "%"});
+            this.comboBox1.Location = new System.Drawing.Point(410, 223);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(80, 37);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // response
+            // 
+            this.response.AutoSize = true;
+            this.response.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.response.Location = new System.Drawing.Point(313, 223);
+            this.response.Name = "response";
+            this.response.Size = new System.Drawing.Size(30, 32);
+            this.response.TabIndex = 4;
+            this.response.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Wide Latin", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(227, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 36);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "CALCULATE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.response);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label response;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Week05/Task01/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task01
+{
+    public partial class Form1 : Form
+    {
+        // % /  - operatorlarindan ibaret calculator yaradin.Calculator-da iki numericUpdown, 
+        //bir combobox ve labellar olsun.Emeliyyat aparacagimiz operatorlari combobox-da saxlayin.
+        //Emeliyyatlari switch/case-le edin ve result-i label-a cixardin. 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void numericUpDown1_ValueChanged_1(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(numericUpDown1.Value);
+            int c = 0;
+            int b = Convert.ToInt32(numericUpDown2.Value);
+            string ch = comboBox1.Text;
+
+            switch (ch)
+            {
+                case "/":
+                    c = a / b;
+                    break;
+                case "%":
+                    c = a % b;
+                    break;
+
+                default:
+                    break;
+            }
+            response.Text = c.ToString();
+        }
+
+        private void numericUpDown2_ValueChanged_1(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(numericUpDown1.Value);
+            int c = 0;
+            int b = Convert.ToInt32(numericUpDown2.Value);
+            string ch = comboBox1.Text;
+
+            switch (ch)
+            {
+                case "/":
+                    if (b != 0)
+                    {
+                        c = a / b;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Yalnish emeliyyat!");
+                    }
+                    break;
+                case "%":
+                    if (b != 0)
+                    {
+                        c = a % b;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Yalnish emeliyyat!");
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            response.Text = c.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(numericUpDown1.Text);
+            int c = 0;
+            int b = Convert.ToInt32(numericUpDown2.Text);
+            string ch = comboBox1.Text;
+
+            switch (ch)
+            {
+                case "/":
+                    if (b != 0)
+                    {
+                        c = a / b;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Yalnish emeliyyat!");
+                    }
+                    break;
+                case "%":
+                    if (b != 0)
+                    {
+                        c = a % b;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Yalnish emeliyyat!");
+                    }
+                    
+                    break;
+
+                default:
+                    break;
+            }
+            response.Text = c.ToString();
+        }
+    }
+}
